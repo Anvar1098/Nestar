e36108a95aeaff324cbe6ee34950af0b35cbabd0
0000 <a0000@Anvars-MacBook-Air.local>
2025-03-04 00:09:27 +0900

    Task

--- apps/nestar-api/src/train.ts
+++ apps/nestar-api/src/train.ts
@@ -1,22 +1,37 @@
-function reduceNestedArray(arr: (number | (number | any[])[])[]): number {
-    let sum = 0;
+// function reduceNestedArray(arr: (number | (number | any[])[])[]): number {
+//     let sum = 0;
 
-    // Loop through each element in the array
-    for (let i = 0; i < arr.length; i++) {
-        const currentItem = arr[i];
+//     // Loop through each element in the array
+//     for (let i = 0; i < arr.length; i++) {
+//         const currentItem = arr[i];
 
-        // If the element is a number, add it to the sum
-        if (typeof currentItem === 'number') {
-            sum += currentItem;
-        }
-        // If the element is an array, call the function recursively
-        else if (Array.isArray(currentItem)) {
-            sum += reduceNestedArray(currentItem);  // Recursively sum the nested array
-        }
-    }
-    console.log('sum:', sum);
-    return sum;
-}
+//         // If the element is a number, add it to the sum
+//         if (typeof currentItem === 'number') {
+//             sum += currentItem;
+//         }
+//         // If the element is an array, call the function recursively
+//         else if (Array.isArray(currentItem)) {
+//             sum += reduceNestedArray(currentItem);  // Recursively sum the nested array
+//         }
+//     }
+//     console.log('sum:', sum);
+//     return sum;
+// }
 
 
-console.log(reduceNestedArray([1, [1, 2, [4]]]));  
+// console.log(reduceNestedArray([1, [1, 2, [4]]]));
+
+//------------------------------------------------------------------------------------------------------------------
+
+function printNumbers(): void {
+    let num: number = 1;
+    const interval: NodeJS.Timeout = setInterval(() => {
+      console.log(num);
+      if (num === 5) {
+        clearInterval(interval);
+      }
+      num++;
+    }, 1000);
+  }
+  
+  printNumbers();
