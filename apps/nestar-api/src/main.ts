e0e01d030900247ff1866a7999cc5b1233189652
0000 <a0000@Anvars-MacBook-Air.local>
2025-03-20 17:34:45 +0900

    fix: update Image uploader

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { MaxFileSizeValidator, ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
+import { graphqlUploadExpress} from "graphql-upload";
+import * as express from 'express';
 
 
 // Global Integrations will be called here
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);     // App =>    Express + NestJs
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new LoggingInterceptor());  
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.enableCors({ origin: true, credentials: true });
+
+  app.use(graphqlUploadExpress({ MaxFileSize: 15000000, maxFiles: 10}));
+  app.use('/uploads', express.static('./uploads'));
+  
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
