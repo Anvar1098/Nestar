251b6105aa05d1f80055dc7d660e74f1661b2487
0000 <a0000@Anvars-MacBook-Air.local>
2025-03-23 00:16:44 +0900

    feat: Update Property business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -11,6 +11,7 @@ import { ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectid } from '../../libs/config';
 import { retry } from 'rxjs';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 
 @Resolver()
@@ -38,5 +39,17 @@ export class PropertyResolver {
       console.log('Query: getProperty');
       const propertyId = shapeIntoMongoObjectid(input);
       return await this.propertyService.getProperty(memberId, propertyId);
+   }
+
+   @Roles(MemberType.AGENT)
+   @UseGuards(RolesGuard)
+   @Mutation((returns) => Property)  
+   public async updateProperty(
+      @Args('input') input: PropertyUpdate,
+      @AuthMember('_id') memberId: ObjectId
+   ): Promise<Property> {
+      console.log('Mutation: updateProperty');
+      input._id = shapeIntoMongoObjectid(input._id);
+      return await this.propertyService.updateProperty(memberId, input);
    }
 }
